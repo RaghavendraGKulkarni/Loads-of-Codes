6b0140c088166ee8b0a2801e6943f00a68c1c97e
Raghavendra Gururaj Kulkarni <70740785+RaghavendraGKulkarni@users.noreply.github.com>
2021-08-20 20:35:01 +0530

    Update Stack Sort.cpp

--- Codes/Stack Sort.cpp
+++ Codes/Stack Sort.cpp
@@ -12,24 +12,25 @@ Program Output Format :- The resulting sorted sequence.
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-void push(vector<int> &A,int x)
+#define ll long long int
+void push(vector<ll> &A,ll x)
 {
 	if(A.size()==0 || x>*A.rbegin())
 		A.push_back(x);
 	else
 	{
-		int temp=*A.rbegin();
+		ll temp=*A.rbegin();
 		A.pop_back();
 		push(A,x);
 		A.push_back(temp);
 	}
 	return;
 }
-void solve(vector<int> &A)
+void solve(vector<ll> &A)
 {
     if(A.size()>0)
 	{
-		int temp=*A.rbegin();
+		ll temp=*A.rbegin();
 		A.pop_back();
 		solve(A);
 		push(A,temp);
@@ -42,8 +43,8 @@ int main()
     ofstream fout("Output.txt");
     ios_base::sync_with_stdio(false);
     fin.tie(NULL),fout.tie(NULL);
-    int i,j,n,ele;
-    vector<int> A;
+    ll i,j,n,ele;
+    vector<ll> A;
     fin>>n;
     for(i=0;i<n;i++)
     {
